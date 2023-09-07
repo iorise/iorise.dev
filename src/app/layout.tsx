fc8f030f1e45b08b85ec22553b57ab66df9c87a3
iorise <riofirmansyah1805@gmail.com>
2023-09-07 11:59:55 +0700

    improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,36 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: `${siteConfig.name} | Fullstack Developer`,
   description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Rio Firmansyah",
+    "Portfolio",
+    "Blog",
+  ],
+  authors: [
+    {
+      name: "Rio Firmansyah",
+      url: "https://github.com/iorise",
+    },
+  ],
+  creator: "Rio Firmansyah",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
